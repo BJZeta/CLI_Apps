90d8e2c36f1c330b9b5a9271a809935b5d5ee8fe
Brandon U <unzueta.brandon@gmai.com>
2019-08-07 21:27:32 -0700

    created connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,15 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+var fs = require('fs');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: '',
+
+    database: 'bamazon_db'
+});
