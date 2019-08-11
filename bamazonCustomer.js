58a86564ff75ab67090c9f59d7e11202f7d28045
Brandon U <unzueta.brandon@gmai.com>
2019-08-11 16:18:19 -0700

    Beginning on the basic layout for inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,33 @@ var connection = mysql.createConnection({
 
     user: 'root',
 
-    password: '',
+    password: 'root',
 
     database: 'bamazon_db'
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    runCustomer();
+});
+
+function runCustomer() {
+    connection.query("SELECT * FROM products",function (err, results) {
+        if (err) throw err;
+        inquirer.prompt({
+            name: "choices",
+            type: "rawlist",
+            message: "What would you like to purchase?",
+            choices: function () {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                    choiceArray.push(results[i].product_name);
+                }
+                return choiceArray;
+            }
+        }).then(function (answer) {
+            console.log('this works');
+            console.log(answer);
+        })
+    })
+}
